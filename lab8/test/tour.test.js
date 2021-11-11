d4b1b412eea8a21295744a4b0d0d8c58c5ba7645
Sergey Golub <sgoluba4261@gmail.com>
2021-11-11 22:01:35 +0300

    fix wait

--- lab8/test/tour.test.js
+++ lab8/test/tour.test.js
@@ -3,10 +3,8 @@ const assert = require('assert');
 const { driver } = require('../pages/TourPage');
 const { tourSiteUrl } = require('../utils/tour.util');
 const { waitTableHotels } = require('../utils/tour.util');
-const { By, until } = require('selenium-webdriver');
 
 describe('Test tour site', function(){
-    this.timeout(50000);
 
     beforeEach(function() {
         TourForm.goToTourSite(tourSiteUrl);
@@ -16,21 +14,15 @@ describe('Test tour site', function(){
         driver.quit();
     });
 
-    it('Search hotels with correct validation', async function() {
+    it('Search hotels with correct validation', () => {
         TourForm.closeWelcomeModal('#fancybox-close');
         TourForm.openHotelsTab('body > div.layout > div.content > div.index-page-block1 > div > div > div > ul > li:nth-child(2) > a');
         TourForm.setTourFormValues();
         TourForm.findHotels();
 
-        const isFoundHotels = !!(await driver.wait(until.elementLocated(By.css('#grid-list > tbody')))
-        .findElements(By.css('tr'))
-            .then(function(hotels){
-            return hotels.length;
-        }));
-        // driver.manage().setTimeouts({implicit: (5000)});
+        const isFoundHotels = waitTableHotels()
+        .then(() => TourForm.getFoundHotelsLength('#grid-list > tbody'));
 
-        // console.log('hotels', hotels);
-        // const isFoundHotels = !!(await TourForm.getFoundHotelsLength('#grid-list > tbody'));
         assert.ok(isFoundHotels);
     })
 })
--- lab8/utils/tour.util.js
+++ lab8/utils/tour.util.js
@@ -2,10 +2,8 @@ const { driver } = require('../pages/TourPage');
 const { By, until } = require('selenium-webdriver');
 const tourSiteUrl = 'https://www.tez-tour.com/';
 
-async function waitTableHotels() {
-    const query = await driver.wait(until.elementLocated(By.css('#grid-list > tbody')));
-    return query.findElements(By.css('tr'))
-    
+function waitTableHotels() {
+    return driver.wait(until.elementLocated(By.css('#grid-list > tbody > tr')), 5000);
 }
 
 module.exports = {
